f56346c1fb99423b51fcacac0f13ce6025f16ad5
suges <suges@Sukesh>
2023-02-13 20:37:45 +0530

    Second commit

---
+++ Jenkins/ZohoCrm2/src/test/java/com/zohoCrm/testscript/CreateCust.java
@@ -0,0 +1,12 @@
+package com.zohoCrm.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCust {
+
+	@Test
+public void testCreateModule() {
+	Reporter.log("CreateModule",true);
+}
+}
